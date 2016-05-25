cc4d51e4015620b194aa4514b0357b23050c254e
Rikki Lanouette <cynicalsaint.lol@gmail.com>
2016-05-25 14:30:28 +0100

    Updated Main.java
    Added support for Hours, Minutes and Seconds

--- src/net/rikelek1/Main.java
+++ src/net/rikelek1/Main.java
@@ -3,7 +3,9 @@ package net.rikelek1;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -16,10 +18,15 @@ import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 
 public class Main extends Application {
-    private String time = "0";
+    private int timeHours, timeMinutes, timeSeconds;
+    private int time = 0;
     private String runCommand = "";
     private Robot robot;
     private Stage window;
+
+    private TextField timeFieldHours = new TextField();
+    private TextField timeFieldMinutes = new TextField();
+    private TextField timeFieldSeconds = new TextField();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -27,50 +34,74 @@ public class Main extends Application {
 
         window = primaryStage;
         window.setTitle("Shutdown app - v0.1");
-        window.setMinWidth(300);
+        window.setMinWidth(625);
         window.setMinHeight(100);
-        window.setMaxWidth(300);
+        window.setMaxWidth(625);
         window.setMaxHeight(100);
 
+        HBox mainHBox = new HBox(5);
         HBox buttons = new HBox(5);
         VBox mainLayout = new VBox(5);
 
-        TextField timeField = new TextField();
-        timeField.setMaxWidth(275);
+        Label labelHours = new Label("Hours");
+        Label labelMinutes = new Label("Minutes");
+        Label labelSeconds = new Label("Seconds");
+
+        timeFieldHours.setMaxWidth(275);
+        timeFieldMinutes.setMaxWidth(275);
+        timeFieldSeconds.setMaxWidth(275);
+
         Button shutdownButton = new Button("Shutdown");
         Button restartButton = new Button("Restart");
         Button abortButton = new Button("Abort");
         Button cancelButton = new Button("Cancel");
 
         shutdownButton.setOnAction(e -> {
-            if(timeField.getText().length() > 0) {
-                time = timeField.getText();
-                execCommand("shutdown");
-            } else {
-                execCommand("shutdown");
-            }
+            setTimes();
+            execCommand("Shutdown");
         });
 
         restartButton.setOnAction(e -> {
-            if(timeField.getText().length() > 0) {
-                time = timeField.getText();
-                execCommand("restart");
-            } else {
-                execCommand("restart");
-            }
+            setTimes();
+            execCommand("Restart");
         });
 
         abortButton.setOnAction(e -> execCommand("abort"));
 
         cancelButton.setOnAction(e -> System.exit(1337));
 
+        mainHBox.getChildren().addAll(labelHours, timeFieldHours, labelMinutes, timeFieldMinutes, labelSeconds, timeFieldSeconds);
+        mainHBox.setAlignment(Pos.CENTER);
         buttons.getChildren().addAll(shutdownButton, restartButton, abortButton, cancelButton);
         buttons.setAlignment(Pos.CENTER);
-        mainLayout.getChildren().addAll(timeField, buttons);
+        mainLayout.getChildren().addAll(mainHBox, buttons);
         mainLayout.setAlignment(Pos.CENTER);
         Scene scene = new Scene(mainLayout);
         window.setScene(scene);
         window.show();
+    }
+
+    private void setTimes() {
+        if(timeFieldHours.getText().equals("")) {
+            timeHours = 0;
+        } else {
+            timeHours = Integer.valueOf(timeFieldHours.getText());
+        }
+
+        if(timeFieldMinutes.getText().equals("")) {
+            timeMinutes = 0;
+        } else {
+            timeMinutes = Integer.valueOf(timeFieldMinutes.getText());
+        }
+
+        if(timeFieldSeconds.getText().equals("")) {
+            timeSeconds = 0;
+        } else {
+            timeSeconds = Integer.valueOf(timeFieldSeconds.getText());
+        }
+
+        time = (timeHours * 3600) + (timeMinutes * 60) + timeSeconds;
+        System.out.println(time);
     }
 
     private void execCommand(String command) {
